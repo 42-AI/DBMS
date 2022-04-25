ac4bfcc0389ad9aa030e58dedbd17fef62e0d51d
Myriam <myriam.anki.pro@gmail.com>
2022-04-25 21:07:24 +0900

    ADD: Interpreter.py

---
+++ Interpreter.py
@@ -0,0 +1,27 @@
+from FileSystemManager import FileSystemManager as fsm
+
+class DatabaseName:
+    db_name = None
+
+class Interpreter:
+    @staticmethod
+    def execute(parsingTree):
+        while parsingTree:
+            if parsingTree.keyword == "USE":
+                DatabaseName.db_name = parsingTree.name
+                print("DATABASE NAME:", DatabaseName.db_name)
+            elif parsingTree.keyword == "CREATE DATABASE":
+                fsm.create_db_dir(parsingTree.name)
+            elif parsingTree.keyword == "DROP DATABASE":
+                fsm.drop_db_dir(parsingTree.name)
+            elif parsingTree.keyword == "CREATE TABLE":
+                print("DATABASE NAME:", DatabaseName.db_name)
+                fsm.create_table_file(parsingTree.name, DatabaseName.db_name, parsingTree.lst)
+            elif parsingTree.keyword == "DROP TABLE":
+                fsm.drop_table_file(parsingTree.name, DatabaseName.db_name)
+            elif parsingTree.keyword == "SHOW DATABASES":
+                pass
+            elif parsingTree.keyword == "SHOW TABLES":
+                pass
+
+            parsingTree = parsingTree.next
