946396d2f27eb592bcc93eb7e0f8383646ff2785
Myriam <myriam.anki.pro@gmail.com>
2022-05-02 22:47:41 +0900

    MOD: FileSystemManager.py > get_fullPath, drop_, get_dbs, get_tables

--- FileSystemManager.py
+++ FileSystemManager.py
@@ -1,18 +1,28 @@
 from pathlib import Path
 import os, sys
 import os.path as path
+import shutil
 _MAIN_PATH_NAME = ".dbms"
 _MAIN_PATH = path.join(os.getcwd(), _MAIN_PATH_NAME)
 
 class FileSystemManager:
 
     @staticmethod
-    def _get_dir_fullPath(dir: str) -> str:
-        return path.join(_MAIN_PATH , dir)
+    def _get_dir_fullPath(_dir: str) -> str:
+        if not _dir:
+            print ("_get_dir_fullPath: _dir is null")
+            return
+        return path.join(_MAIN_PATH , _dir)
 
     @staticmethod
-    def _get_file_fullPath(file: str, dir: str) -> str:
-        return path.join(FileSystemManager._get_dir_fullPath(dir), file)
+    def _get_file_fullPath(file: str, _dir: str) -> str:
+        if not _dir:
+            print ("_get_dir_fullPath: _dir is null")
+            return
+        if not file:
+            print ("_get_dir_fullPath: file is null")
+            return
+        return path.join(FileSystemManager._get_dir_fullPath(_dir), file)
 
     @staticmethod
     def create_main_directory():
@@ -45,6 +55,10 @@ class FileSystemManager:
     @staticmethod
     def drop_table_file(table_name: str, db_name: str):
         table_fullPath = FileSystemManager._get_file_fullPath(table_name, db_name)
+        db_fullPath = FileSystemManager._get_dir_fullPath(db_name)
+        if not table_fullPath:
+            print("no valid table")
+            return
         if not path.exists(table_fullPath):
             print(f"Error: table {table_name} doesn't exists.", file=sys.stderr)
             return
@@ -55,13 +69,25 @@ class FileSystemManager:
 
     @staticmethod
     def drop_db_dir(db_name: str):
-        db_dir_fullPath = FileSystemManager._get_file_fullPath(db_name)
+        db_dir_fullPath = FileSystemManager._get_dir_fullPath(db_name)
+        if not db_dir_fullPath:
+            print("no valid DB used")
+            return
         if not path.isdir(db_dir_fullPath):
             print(f"Error: database {db_name} doesn't exists.", file=sys.stderr)
             return
-        os.rmdir(db_dir_fullPath)
+        shutil.rmtree(db_dir_fullPath)
 
     @staticmethod
     def get_dbs() -> list :
-        return os.listdir(_MAIN_PATH)
+        elements = os.listdir(_MAIN_PATH)
+        dbs = []
+        for element in elements:
+            if path.isdir(FileSystemManager._get_file_fullPath(element, _MAIN_PATH)):
+                dbs.append(element)
+        return dbs
+
+    @staticmethod
+    def get_tables(db_name) -> list :
+        return os.listdir(FileSystemManager._get_dir_fullPath(db_name))
         
