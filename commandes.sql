2fbcac3a4fb021a764a5ff92ee971e6ade2c4ee0
Myriam <myriam.anki.pro@gmail.com>
2022-09-08 14:17:52 +0200

    ADD: commandes.sql

---
+++ commandes.sql
@@ -0,0 +1,54 @@
+-- MySQL or mariaDB
+
+CREATE DATABASE db_name;
+DROP DATABASE db_name;
+
+CREATE TABLE customers (
+    customer_id int,
+    name varchar(255),
+    age int
+);
+-- Types:
+-- char(n)
+-- varchar(n)
+-- integer
+-- real (6 chiffre de precision)
+-- serial (auto increment==> pour les id/primary key)
+
+-- Keywords:
+-- PRIMARY KEY
+-- NOT NULL
+-- NULL
+-- LIMIT 10
+
+DROP TABLE db_name;
+
+-- ALTER TABLE => add column:
+ALTER TABLE customers
+ADD surname varchar(255);
+
+-- => DROP COLUMN:
+ALTER TABLE table_name
+  DROP COLUMN column_name;
+
+
+-- DESCRIBE provides information about the columns in a table
+DESCRIBE tbl_name;
+
+-- SHOW DATABASES lists the databases on the MariaDB server host
+SHOW DATABASES;
+USE db_name;
+SHOW TABLES;
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+DELETE FROM table_name WHERE condition; 
+
+Ponctuation: '()', ',', ';', '+' -- le + pour les concatenation dans les alias aussi
+OPERATEURS: < > <= >= = <> != 
+LOGICAL: AND, OR
+
+SELECT (DISTINCT) value AS alias,
+                  value + '_' + value AS alias
+FROM TABLE AS t1
+WHERE ...
