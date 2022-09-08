32a435b550d2da41d0f528e3dd973d2f542689af
Myriam <myriam.anki.pro@gmail.com>
2022-09-08 16:36:51 +0200

    REM: create_test_file.txt

--- Interpreter.py
+++ Interpreter.py
@@ -11,16 +11,16 @@ class Interpreter:
     def execute(parsingTree):
         keyword_functions = {
             # "DROP DATABASE": lambda node: fsm.drop_db_dir(node.name),
-            # "DROP TABLE": lambda node: fsm.drop_table_file(node.name, node.db_name),
-            # "CREATE TABLE": lambda node: fsm.create_table_file(node.name, node.db_name, node.lst),
-            "CREATE DATABASE": lambda node: fsm.create_db_dir(node.db_name),
+            # "DROP TABLE": lambda node: fsm.drop_table_file(node.name, node.data),
+            # "CREATE TABLE": lambda node: fsm.create_table_file(node.name, node.data, node.lst),
+            "CREATE DATABASE": lambda node: fsm.create_db_dir(node.data),
             "SHOW DATABASES": lambda node: Show.databases(),
-            "SHOW TABLES": lambda node: Show.tables(node.db_name),
+            "SHOW TABLES": lambda node: Show.tables(node.data),
         }
         while parsingTree:
-            print(f"{colors.WARNING}DATABASE NAME: {DatabaseName.db_name}{colors.ENDC}")
+            print(f"{colors.WARNING}DATABASE NAME: {DatabaseName.data}{colors.ENDC}")
             if parsingTree.keyword == "USE":
-                DatabaseName.db_name = parsingTree.db_name
+                DatabaseName.data = parsingTree.data
             elif parsingTree.keyword in keyword_functions.keys():
                 keyword_functions[parsingTree.keyword](parsingTree)
             parsingTree = parsingTree.next
