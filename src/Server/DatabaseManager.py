ef257b0d0f537b7d9d45df3a8b8aa2d8997d7eca
clovis_42 <clmenega@student.42.fr>
2022-09-12 17:53:02 +0200

    Refactor: create SreverTools, move DB dirs function in DatabaseManager

---
+++ src/Server/DatabaseManager.py
@@ -0,0 +1,43 @@
+import os.path as path
+import os, sys
+import src.Server.ServerTools as Tools
+
+class DatabaseManager:
+
+    @staticmethod
+    def create_db_dir(db_name: str):
+        DatabaseManager._create_main_directory()
+        db_fullPath = path.join(Tools.MAIN_PATH, db_name)
+        if path.isdir(path.expanduser(db_fullPath)):
+            print(f"Error: database {db_name} already exists.", file=sys.stderr)
+            return
+        os.mkdir(path.expanduser(db_fullPath))
+
+    @staticmethod
+    def drop_db_dir(db_name: str):
+        db_dir_fullPath = ServerTools.get_dir_fullPath(db_name)
+        if not db_dir_fullPath:
+            print("no valid DB used")
+            return
+        if not path.isdir(db_dir_fullPath):
+            print(f"Error: database {db_name} doesn't exists.", file=sys.stderr)
+            return
+        shutil.rmtree(db_dir_fullPath)
+
+    @staticmethod
+    def get_dbs():
+        dir_list = os.listdir(os.getcwd())
+        if Tools.MAIN_PATH_NAME not in dir_list:
+            print(f"{colors.BOLD}Empty set{colors.ENDC}")
+            return
+        elements = os.listdir(Tools.MAIN_PATH)
+        dbs = []
+        for element in elements:
+            if path.isdir(ServerTools.get_file_fullPath(element, Tools.MAIN_PATH)):
+                dbs.append(element)
+        return dbs
+
+    @staticmethod
+    def _create_main_directory():
+        if not path.isdir(path.expanduser(Tools.MAIN_PATH)):
+            os.mkdir(path.expanduser(Tools.MAIN_PATH))
--- src/Server/FileSystemManager.py
+++ src/Server/FileSystemManager.py
@@ -1,31 +1,12 @@
 from pathlib import Path
 import os, sys
+from src.Server.DatabaseManager import DatabaseManager
 import os.path as path
 import shutil
 from src.utils import colors
-_MAIN_PATH_NAME = ".dbms"
-_MAIN_PATH = path.join(os.getcwd(), _MAIN_PATH_NAME)
 
 class FileSystemManager:
 
     @staticmethod
-    def _get_dir_fullPath(_dir: str):
-        if not _dir:
-            print ("_get_dir_fullPath: _dir is null")
-            return
-        return path.join(_MAIN_PATH , _dir)
-
-    @staticmethod
-    def _get_file_fullPath(file: str, _dir: str):
-        if not _dir:
-            print ("_get_dir_fullPath: _dir is null")
-            return
-        if not file:
-            print ("_get_dir_fullPath: file is null")
-            return
-        return path.join(FileSystemManager._get_dir_fullPath(_dir), file)
-
-    @staticmethod
-    def create_main_directory():
-        if not path.isdir(path.expanduser(_MAIN_PATH)):
-            os.mkdir(path.expanduser(_MAIN_PATH))
+    def create_db(db_name):
+        DatabaseManager.create_db_dir(db_name)
