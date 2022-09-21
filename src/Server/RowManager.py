3727aef9670d34ff4944d65daa010def72ca8810
clovis_42 <clmenega@student.42.fr>
2022-09-21 14:55:59 +0200

    feat: use csvHandler

--- src/Server/RowManager.py
+++ src/Server/RowManager.py
@@ -1,5 +1,6 @@
 from src.Server.ServerTools import ServerTools
 import src.Server.ServerTools as Tools
+from src.Server.csvHandler import csvHandler
 
 class RowManager:
 
@@ -13,26 +14,16 @@ class RowManager:
         RowManager.db_name = db_name
         RowManager.table_name = table_name
         RowManager.content = content
+        RowManager.appendLine()
 
     ########## Insert: Private #######
     @staticmethod
-    def write_table():
-        #insert data
+    def appendLine():
+        file_full_paht = Tools.get_file_full_path(RRowManager.table_name, owManager.db_name, "data")
+        csv_handler = csvHandler(file_full_paht)
+        data = csv_handler.get_content()
+        data.append(RowManager.content)
+        csv_handler.set_content(data)
 
-    @staticmethod
-    def build_row(row_dict: dict):
-        row_str = '';
-        structure = get_tabel_structure()
-        for field in structure:
-            row_str =+ str(row_dict[field]) + ','
-        return fields[:-2] + '\n'
-
-    ########## Get: Private #######
+    ########## Get: Private ##########
         @saticmethod
-        def get_tabel_structure():
-            table_data_file_full_path = ServerTools.get_file_full_path(RowManager.table_name, RowManager.db_name, "data")
-            f = open(table_data_file_full_path, "r")
-            lines = f.readlines()
-            line = lines[0]
-            structure = line.split(",")
-            return structure
