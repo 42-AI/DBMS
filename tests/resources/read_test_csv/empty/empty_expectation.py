dc11344b116ab4b6d6d9f8774c414c2fb1c98900
clovis_42 <clmenega@student.42.fr>
2022-09-24 16:39:32 +0200

    Add resources to test empty csv

---
+++ tests/resources/read_test_csv/empty/empty_expectation.py
@@ -0,0 +1,7 @@
+empty_test_array = [
+    ("empty_1.csv", ["first", "second", "third", "forth", "fifth", "sixth"]),
+    ("empty_2.csv", ["Identifier", "Access code", "Recovery code", "First name", "Last name", "Department", "Location"]),
+    ("empty_3.csv", ["Username", "Identifier", "First name", "Last name"]),
+    ("empty_4.csv", ["Username", "Identifier", "One-time password", "Recovery code", "First name", "Last name", "Department", "Location"])
+]
+
