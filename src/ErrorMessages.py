806f4ae9bbee078ee30688017f6d3589e1b0162c
clovis_42 <clmenega@student.42.fr>
2022-10-06 16:01:32 +0200

    Feat: add exception raising in FileSystemManager

--- src/ErrorMessages.py
+++ src/ErrorMessages.py
@@ -1,4 +1,10 @@
 class ErrorMessages:
+
+    #### DataBasea ####
     DB_ALREADY_EXIST = "Error Database already exist"
     DB_DOES_NOT_EXIST = "Error Database does not exist"
     NO_DBS_DIR = "Error Main directory does not exist"
+
+    #### FileSystemManager ####
+    NO_DB_SELECTED = "Error no database is selected"
+    NO_TABLE_SELECTED = "Error no table is selected"
