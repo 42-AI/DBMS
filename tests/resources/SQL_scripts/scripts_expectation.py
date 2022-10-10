2d4192d53f736e363db13d25be914cfd477a30b0
clovis_42 <clmenega@student.42.fr>
2022-10-10 16:12:23 +0200

    Test: add integration scripts

---
+++ tests/resources/SQL_scripts/scripts_expectation.py
@@ -0,0 +1,4 @@
+script_expectation_array = [
+    ("create_db.sql", "+-----------+\n| Database  |\n+-----------+\n| character |\n+-----------+\n5 rows in set\n"),
+    ("create_table.sql", "+------------------+\n| Tables_in_sports |\n+------------------+\n| users            |\n| places           |\n| activity         |\n+------------------+\n7 rows in set\n")
+]
