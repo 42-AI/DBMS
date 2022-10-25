cab4bc27940ccaac4bdc8376328276a2c388fca2
clmenega <clmenga@student.42.fr>
2022-10-25 09:12:23 -0400

    Fix: change expected output in integration tests

--- tests/resources/SQL_scripts/scripts_expectation.py
+++ tests/resources/SQL_scripts/scripts_expectation.py
@@ -1,4 +1,4 @@
 script_expectation_array = [
-    ("create_db.sql", "+-----------+\n| Database  |\n+-----------+\n| character |\n+-----------+\n5 rows in set\n"),
-    ("create_table.sql", "+------------------+\n| Tables_in_sports |\n+------------------+\n| users            |\n| places           |\n| activity         |\n+------------------+\n7 rows in set\n")
+    ("create_db.sql", "+-----------+\n| Database  |\n+-----------+\n| character |\n+-----------+\n1 rows in set\n\n"),
+    ("create_table.sql", "+------------------+\n| Tables_in_sports |\n+------------------+\n| activity         |\n| places           |\n| users            |\n+------------------+\n3 rows in set\n\n")
 ]
